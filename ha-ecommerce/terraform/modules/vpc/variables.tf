b0a6b09fb86916620fca04a7e023741246b60edd
Benny Maliti <161900365+bennymaliti@users.noreply.github.com>
2025-09-13 11:57:39 +0100

    Add VPC variables for Terraform module

--- ha-ecommerce/terraform/modules/vpc/variables.tf
+++ ha-ecommerce/terraform/modules/vpc/variables.tf
@@ -1,1 +1,35 @@
+#===========================================
+# terraform/modules/vpc/variables.tf
+#===========================================
 
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"  
+  type        = list(string)
+}
+
+variable "project_name" {
+  description = "Project name for resource naming"
+  type        = string
+  default     = "ha-ecommerce"
+}
+
+variable "tags" {
+  description = "Tags to apply to resources"
+  type        = map(string)
+  default     = {}
+}
